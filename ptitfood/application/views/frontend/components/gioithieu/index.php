086a9e9fb09b7dca223e04aed1d6bcd93f65bfc3
dovankha <dovankha.tlvn@gmail.com>
2022-04-04 23:12:53 +0700

    First update - by Kha

---
+++ ptitfood/application/views/frontend/components/gioithieu/index.php
@@ -0,0 +1,14 @@
+<center>
+	<h3>• Website đồ án môn CNPT được chạy trên localhost <br />
+		Tên đồ án: PTITFOODX <br />
+		• Được thực hiện bởi sinh viên:
+		<br />
+		○ ĐỖ VĂN KHA - N19DCAT041
+		<br />
+		○ NGUYỄN CHÍ BẢO - N19DCAT006
+		<br />
+		○ HUỲNH ĐỨC THẮNG - N19DCAT081
+		<br />
+		• GV Hướng Dẫn: Nguyễn Thị Bích Nguyên
+	</h3>
+</center>
